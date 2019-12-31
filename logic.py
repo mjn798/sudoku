799a135a2d7073efc59e9837c2c8f4593dd026df
Michael J. Neumann <53601513+mjn798@users.noreply.github.com>
2019-12-31 13:44:42 +0100

    improved logic

--- logic.py
+++ logic.py
@@ -1,32 +1,32 @@
 class Logic:
 
-    # track the number of iterations / numbers set as a performance indicator
+    # track the number of iterations as a performance indicator
     tracks = 0
 
     # global cache for each individual cell's neighbours
     neighbourCache = list()
 
-    # prepare the neighbourCache for each individual cell - dictionary with indices for row, column, block and a combination of all of them
+    # prepare the neighbourCache for each individual cell - with indices for row, column, block and a combination
     def prepareNeighbourCache(self):
-        # for each field in the sudoku
+        # for each cell in the sudoku
         for index in range(81):
-            # create new sets for candidate values - each candidate should only appear once
+            # create new distinct sets for neighbours
             neighboursRow = set()
             neighboursColumn = set()
             neighboursBlock = set()
-            # each field has 9 neighbours per row, column and block
+            # each cell has 9 neighbours per row, column and block (including itself)
             for i in range(9):
                 neighboursRow.add(((index // 9) * 9) + i)
                 neighboursColumn.add((index % 9) + (i * 9))
                 neighboursBlock.add(((index // 27) * 27) + (((index % 9) // 3) * 3) + (i % 3) + ((i // 3) * 9))
-            # add neighbours to the cache and remove the index - the index itself is not its own neighbour
+            # add neighbours to the cache and remove the index, as the cell itself is not its own neighbour
             self.neighbourCache.append({"row": list(neighboursRow.difference({index})), "column": list(neighboursColumn.difference({index})), "block": list(neighboursBlock.difference({index})), "all": list(neighboursRow.union(neighboursColumn, neighboursBlock).difference({index}))})
 
     # prepare the puzzle
     def preparePuzzle(self, puzzle):
-        # add all candidates (1-9) to individual cells
+        # add all candidates (1-9) to all individual cells
         cells = list("123456789" for i in range(81))
-        # for all non . characters in the puzzle, set the number
+        # for all non . characters in the puzzle, set the known number
         for index in range(81):
             if puzzle[index] != ".":
                 cells = self.setValue(cells, index, puzzle[index])
@@ -37,60 +37,68 @@ class Logic:
     def setValue(self, cells, index, value):
         # is it allowed to set the value at the index?
         if self.isAllowedToSet(cells, index, value):
-            # set the value, making it the only possible candidate and remove the value from all neighbour cells
+            # set the value, making it the only possible candidate
             cells[index] = value
-            cells = self.removeFromNeighbours(cells, index, value)
-            # searching for hidden singles here would improve the number of tracks, but increase computing time significantly
+            # remove the value from its neighbours' candidates
+            for neighbour in self.neighbourCache[index]["all"]:
+                # only look at the cell if the value is still a potential candidate
+                if value in cells[neighbour]:
+                    cells[neighbour] = cells[neighbour].replace(value, "")
+                    # naked single - if there is only one candidate left, it must be the known value
+                    if len(cells[neighbour]) == 1: self.setValue(cells, neighbour, cells[neighbour])
         return cells
 
-    # iterate through all neighbour cells and remove the candidate by blanking it
-    def removeFromNeighbours(self, cells, index, value):
-        newSingles = list()
-        for neighbour in self.neighbourCache[index]["all"]:
-            # only look at the cell if the value is still a potential candidate
-            if value in cells[neighbour]:
-                cells[neighbour] = cells[neighbour].replace(value, "")
-                # if there's only one value left as a candidate, it must be the value - set it later
-                if len(cells[neighbour]) == 1: newSingles.append(neighbour)
-        # set known value for cells that had all candidates removed and now have only a single candidate left
-        for newSingle in newSingles: cells = self.setValue(cells, newSingle, cells[newSingle])
-        return cells
-
-    # find hidden singles wrapper - search for row, column and block in all cells
+    # find hidden singles - if a candidate only appears once per row, column or block it must be the known value
     def findHiddenSingles(self, cells):
-        for i in range(81):
-            if (len(cells[i]) > 1):
-                for cacheType in ["row", "column", "block"]: cells = self.findHiddenSingleSearch(cells, i, cacheType)
+        # check all possible cells
+        for index in range(81):
+            # only consider if it is not a known value - there must be more than one candidate left
+            if len(cells[index]) > 1:
+                # search in each row, column and block individually
+                for cacheType in ["row", "column", "block"]:
+                    # variable to compare to the actual value in the index to its neighbours
+                    checkUnit = cells[index]
+                    # check all candidate values in all neighbour cells
+                    for neighbour in self.neighbourCache[index][cacheType]:
+                        for value in cells[neighbour]:
+                            # remove all the neighbour's candidate numbers from the comparison
+                            checkUnit = checkUnit.replace(value, "")
+                    # if there is only one candidate left we have found a hidden single
+                    if len(checkUnit) == 1: cells = self.setValue(cells, index, checkUnit); break
         return cells
 
-    # find hidden singles - if a value only appears once per row, column or block it must be the actual value
-    def findHiddenSingleSearch(self, cells, index, cacheType):
-        # check the neighbours for the index
-        for neighbour in self.neighbourCache[index][cacheType]:
-            # only consider if it's not a known value, so more than one candidate
-            if len(cells[neighbour]) > 1:
-                # checkUnit holds all the candidates in a cell
-                checkUnit = cells[neighbour]
-                # check the neighbours for the index's neighbour
-                for neighboursneighbour in self.neighbourCache[neighbour][cacheType]:
-                    # remove each candidate from the checkUnit by blanking the values
-                    for value in cells[neighboursneighbour]: checkUnit = checkUnit.replace(value, "")
-                # if there's only one number left, it must be the value - so set it
-                if len(checkUnit) == 1: cells = self.setValue(cells, neighbour, checkUnit)
+    # find naked pairs - if a candidate pair appears in only two cells per row, column or block it must be there and cannot be a candidate for any other neighbours
+    def findNakedPairs(self, cells):
+        # check all possible cells
+        for index in range(81):
+            # only consider if there are exactly two candidates left
+            if len(cells[index]) == 2:
+                # search in each row, column and block individually
+                for cacheType in ["row", "column", "block"]:
+                    # check all the neighbours of the cell for matches
+                    for neighbour in self.neighbourCache[index][cacheType]:
+                        # is it a match / exactly the same two values?
+                        if cells[index] == cells[neighbour]:
+                            # all other neighbours get the two candidates removed
+                            for affected in list(set(self.neighbourCache[index][cacheType]).difference({neighbour})):
+                                for value in cells[index]:
+                                    cells[affected] = cells[affected].replace(value, "")
+                                # if there's only one candidate left, the affected cell has now a known value
+                                if len(cells[affected]) == 1: cells = self.setValue(cells, affected, cells[affected])
         return cells
 
     # is it allowed to set a certain value at a certain index in the sudoku?
     def isAllowedToSet(self, cells, index, value):
-        # return true if the 1) value is a candidate AND 2) the value is not already set in any neighbour cell
+        # return true if the 1) value is a candidate AND 2) the value is not already a known value in any neighbour cell
         return (value in cells[index]) and all(value != cells[neighbour] for neighbour in self.neighbourCache[index]["all"])
 
     # is there a contradiction in the sudoku, e.g. empty cells or duplicate known values?
     def hasContradiction(self, cells):
         # for each field in the sudoku
         for i in range(81):
-            # if there are any empty cells, it's a contradiction
+            # if there are any empty cells, it is a contradiction
             if len(cells[i]) == 0: return True
-            # if there are any two neighbours with the same known value, it's a contradiction
+            # if there are any two neighbours with the same known value, it is a contradiction
             if len(cells[i]) == 1:
                 for neighbour in self.neighbourCache[i]["all"]:
                     if cells[neighbour] == cells[i]: return True
@@ -99,25 +107,24 @@ class Logic:
 
     # recursive backtracking function
     def solveSudoku(self, cells):
-        # before doing anything, resolve any hidden singles
+        # before doing anything, optimize the cells
         tempcells = list()
         while cells != tempcells:
             cells = self.findHiddenSingles(cells)
+            cells = self.findNakedPairs(cells)
             tempcells = cells
-        # if there's a contradiction return False - something's wrong with the solution
-        if self.hasContradiction(cells): return False
+        # if there is a contradiction return False - something is wrong with the solution
+        if self.hasContradiction(cells):
+            return False
         # if all cells only have one candidate left, the puzzle is solved
         if all(len(cell) == 1 for cell in cells):
             self.solved = cells.copy()
             return True
-        # get the next open position for setting a value by checking field lengths and take the shortest one (> 1)
+        # get the next open position for setting a value by checking field lengths and take the shortest one
         position = -1
-        position_length = 11
         for i in range(81):
-            if (len(cells[i]) > 1) and (len(cells[i]) < position_length):
-                position = i
-                position_length = len(cells[i])
-            if position_length == 2: break
+            if (len(cells[i]) == 2): position = i; break
+            elif (len(cells[i]) < max(2, len(cells[position]))): position = i
         # check all possible candidates for the current position
         for value in cells[position]:
             # if there are no conflicts with neighbour cells, set the number at the position and continue in the tree
